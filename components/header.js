f3ba333172c6e9df04e5281bea474da493f0c13a
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2022-01-04 10:54:51 -0600

    Header

---
+++ components/header.js
@@ -0,0 +1,20 @@
+const template = document.createElement('template')
+template.innerHTML = `
+  <style>
+    background-color: lightgreen;
+  </style>
+
+  <header>
+    <h1><a href="/">Jakub Šťastný</a></h1>
+  </header>`
+
+customElements.define('Header',
+  class extends HTMLElement {
+    constructor() {
+      super()
+
+      const shadowRoot = this.attachShadow({mode: 'open'})
+        .appendChild(template.content.cloneNode(true))
+    }
+  }
+)
