9ff2ef9abd7c659ae39a6eb37ee395ab4e30736b
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2022-01-14 13:25:23 -0600

    Propagate query string

--- js/propagate-qs.js
+++ js/propagate-qs.js
@@ -1,5 +1,14 @@
+/*
+ * Propagate all query string parameters across.
+ *
+ * For local development add .html, so it resolves in the WorkingCopy server.
+ */
 window.addEventListener('DOMContentLoaded', (event) => {
   document.querySelectorAll('a').forEach(a => {
-    console.log(a)
+    if (location.host.match(/^\d{3}\.\d+\.\d+\.\d+:\d+$/)) {
+      a.href = `${a.href}.html${location.search}`
+    } else {
+      a.href = `${a.href}${location.search}`
+    }
   })
 })
