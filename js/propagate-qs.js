d28420563267ad5771e609fe23f8625417d6d3a4
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2022-01-15 03:06:15 -0600

    Improving URL rewrites

--- js/propagate-qs.js
+++ js/propagate-qs.js
@@ -3,12 +3,24 @@
  *
  * For local development add .html, so it resolves in the WorkingCopy server.
  */
+
+function rewriteLink(a, destination) {
+  console.log(`URL ${a.href} -> ${destination}`)
+  a.href = destination
+}
+
 window.addEventListener('DOMContentLoaded', (event) => {
   document.querySelectorAll('a').forEach(a => {
     if (location.host.match(/^\d{3}\.\d+\.\d+\.\d+:\d+$/)) {
-      a.href = `${a.href}.html${location.search}`
+      if (a.href.match(/^\/$/)) {
+        // WorkingCopy server doesn't know how to deal with /.
+        // FIXME ShadowRoot in header not matching.
+        rewriteLink(a, `index.html${location.search}`)
+      } else {
+        rewriteLink(a, `${a.href}.html${location.search}`)
+      }
     } else {
-      a.href = `${a.href}${location.search}`
+      rewriteLink(a, `${a.href}${location.search}`)
     }
   })
 })
