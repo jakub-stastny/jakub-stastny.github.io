4b6982fe6b3cc48f11857e2f7ddc0deffdfee1dc
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2022-01-22 06:31:42 -0600

    Refactoring

--- js/components/components.js
+++ js/components/components.js
@@ -1,40 +1,62 @@
 import { rewriteLinks } from "/js/propagate-qs.js"
-import { inDev } from "/js/helpers.js"
+import { inDev, tag } from "/js/helpers.js"
 
 function defineComponent(name, callback) {
   customElements.define(name,
     class extends HTMLElement {
       constructor() {
         super()
-      
-        const template = document.createElement('template')
-        const response = fetch(`/js/components/templates/${name}.html`, {headers: {'Content-Type': 'text/plain'}})
 
-        response.then((res) => res.text().then((text) => {
-          template.innerHTML = text
+        this.fetchTemplate().then((res) => res.text().then((text) => {
+          const template = tag('template', {innerHTML: text})
           this.attachShadow({mode: 'open'}).
             appendChild(template.content.cloneNode(true))
 
-          if (callback) callback(this.shadowRoot, this)
-          rewriteLinks(this.shadowRoot)
+          this.callback(this)
         }))
+      }
+
+      callback(customElement) {
+        if (callback) callback(customElement.shadowRoot, customElement)
+        rewriteLinks(customElement.shadowRoot)
+      }
+
+      fetchTemplate() {
+        return fetch(`/js/components/templates/${name}.html`, {headers: {'Content-Type': 'text/plain'}})
+      }
+
+      attributeChangedCallback() {
+        console.log("Change", this, arguments)
+        this.callback(this)
       }
     }
   )
 }
 
-function hideInProduction() {
-  return (shadowRoot) => {
-    console.log(`Hiding in production: ${!inDev()}`)
-    if (!inDev()) shadowRoot.replaceChildren()
-  }
+function hideInProduction(shadowRoot) {
+  console.log(`Hiding in production: ${!inDev()}`)
+  if (!inDev()) shadowRoot.replaceChildren()
 }
 
-defineComponent('site-header', (shadowRoot) => {
-  // TODO: Move into the template.
-  if (location.pathname.match(/^\/(index\.html)?$/)) {
-    shadowRoot.querySelector('#link').removeAttribute('href')
-  }
+/* Components */
+defineComponent('contact-card')
+defineComponent('copy-about-me')
+
+defineComponent('copy-healing', (shadowRoot, customElement) => {
+  const link = customElement.getAttribute('leader')
+  shadowRoot.querySelector('leader-image').setAttribute('src', link)
+})
+
+defineComponent('corner-ribbon', (shadowRoot, customElement) => {
+  const link = customElement.getAttribute('link')
+  shadowRoot.querySelector('a').setAttribute('href', link)
+})
+
+defineComponent('cta-button')
+
+defineComponent('leader-image', (shadowRoot, customElement) => {
+  const link = customElement.getAttribute('src')
+  shadowRoot.querySelector('img').setAttribute('src', link)
 })
 
 defineComponent('site-footer', (shadowRoot) => {
@@ -54,19 +76,13 @@ defineComponent('site-footer', (shadowRoot) => {
   }
 })
 
-defineComponent('stay-in-touch')
-defineComponent('leader-image', (shadowRoot, customElement) => {
-  const link = customElement.getAttribute('src')
-  shadowRoot.querySelector('img').src = link
-})
-defineComponent('user-testimonials', hideInProduction())
-defineComponent('user-testimonial')
-defineComponent('cta-button')
-defineComponent('contact-card')
-defineComponent('corner-ribbon', (shadowRoot, customElement) => {
-  const link = customElement.getAttribute('link')
-  shadowRoot.querySelector('a').href = link
+defineComponent('site-header', (shadowRoot) => {
+  // TODO: Move into the template.
+  if (location.pathname.match(/^\/(index\.html)?$/)) {
+    shadowRoot.querySelector('#link').removeAttribute('href')
+  }
 })
 
-defineComponent('copy-healing')
-defineComponent('copy-about-me')
+defineComponent('stay-in-touch')
+defineComponent('user-testimonials', hideInProduction)
+defineComponent('user-testimonial')
