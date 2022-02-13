354dfc01962978acd4a73c327371f4c2fcf2f50d
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2022-02-13 09:47:53 -0600

    Move JS from callbacks to custom scripts

--- js/components.js
+++ js/components.js
@@ -14,11 +14,13 @@ function renderTemplate(string, context) {
 function generateTagClone(varName, templateName, script) {
   const lines = [
     "import { _init } from '/js/component-helpers.js'",
+    "import { rewriteLinks } from '/js/propagate-qs.js'",
     `const customExports = _init(${varName}, '${templateName}', '${script.getAttribute('name')}')`,
     "Object.entries(customExports).map(([ fnName, fn ]) => window[fnName] = fn)", // I think this will rewrite the one on window, we need local scope, maybe using eval.
     script.text.
       replace(/customElement/g, varName).
-      replace(/shadowRoot/g, `${varName}.shadowRoot`)]
+      replace(/shadowRoot/g, `${varName}.shadowRoot`),
+    `rewriteLinks(${varName}.shadowRoot)`]
 
   return tag('script', {type: 'module', text: lines.join("\n")})
 }
@@ -50,7 +52,7 @@ function defineComponent(name, callback) {
           callback(customElement.shadowRoot, customElement)
         }
 
-        rewriteLinks(customElement.shadowRoot)
+        // rewriteLinks(customElement.shadowRoot)
       }
 
       fetchTemplate() {
@@ -74,50 +76,17 @@ function hideInProduction(shadowRoot) {
 /* Components */
 defineComponent('contact-card')
 defineComponent('copy-about-me')
-
-defineComponent('copy-healing', (shadowRoot, customElement) => { // TODO: move to the template.
-  window.e = customElement
-  const link = customElement.getAttribute('leader')
-  shadowRoot.querySelector('leader-image').setAttribute('src', link)
-})
-
-defineComponent('corner-ribbon', (shadowRoot, customElement) => { // TODO: move to the template.
-  const link = customElement.getAttribute('link')
-  shadowRoot.querySelector('a').setAttribute('href', link)
-})
-
+defineComponent('copy-healing')
+defineComponent('corner-ribbon')
 defineComponent('cta-button')
 defineComponent('debug-info', hideInProduction)
-
-defineComponent('leader-image', (shadowRoot, customElement) => { // TODO: move to the template.
-  const link = customElement.getAttribute('src')
-  shadowRoot.querySelector('img').setAttribute('src', link)
-})
-
-defineComponent('site-footer', (shadowRoot) => {
-  // TODO: Move into the template.
-  function hideElement(element) {
-    console.log("Hidding", element)
-    element.style.display = 'none'
-  }
-
-  function isApplePlatform() {
-    return navigator.userAgent.match(/iPad|iPhone|iPod|/) || navigator.platform === 'MacIntel'
-  }
-
-  /* Hide iMessage on non-Apple platforms. */
-  if (!isApplePlatform()) {
-    shadowRoot.querySelectorAll('.imessage').forEach(element => hideElement(element))
-  }
-})
-
-defineComponent('site-header', (shadowRoot) => {
-  // TODO: Move into the template.
-  if (location.pathname.match(/^\/(index\.html)?$/)) {
-    shadowRoot.querySelector('#link').removeAttribute('href')
-  }
-})
-
+defineComponent('feedback-form')
+defineComponent('leader-image')
+defineComponent('repeated-sessions')
+defineComponent('site-footer')
+defineComponent('site-header')
 defineComponent('stay-in-touch')
-defineComponent('user-testimonials', hideInProduction)
+defineComponent('thank-you')
 defineComponent('user-testimonial')
+defineComponent('user-testimonials', hideInProduction)
+defineComponent('write-testimonial')
