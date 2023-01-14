7eb92be6006287092203207289e462d9893a781b
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-01-14 16:13:25 +0000

    The current route is no longer clickable

--- js/layout.js
+++ js/layout.js
@@ -1,2 +1,15 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
 const year = new Date().getFullYear()
-document.querySelector(".year").innerText = year.toString()
+$(".year").innerText = year.toString()
+
+const currentRoute = location.pathname.replace(/\.html$/, "")
+$$("nav a").forEach((a) => {
+  const route = new URL(a.href)
+  if (route.pathname === currentRoute) {
+    a.classList.add("inactive")
+    console.log(a)
+    window.a = a
+  }
+})
