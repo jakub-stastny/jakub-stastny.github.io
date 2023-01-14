3254bce1c4b778a84c9d7079dd68f7dbef795cc5
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-01-14 17:14:46 +0000

    Use history.pushState() rather than refreshing the page

--- js/layout.js
+++ js/layout.js
@@ -1,15 +1,51 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
+// Show current year in the copyright.
 const year = new Date().getFullYear()
 $(".year").innerText = year.toString()
 
-const currentRoute = location.pathname.replace(/\.html$/, "")
+// Disable current route in navigation.
+function makeCurrentRouteNotClickable() {
+  const currentRoute = location.pathname.replace(/\.html$/, "")
+  $$("nav a").forEach((a) => {
+    const route = new URL(a.href.replace(/\.html$/, ""))
+    if (route.pathname === currentRoute) {
+      a.classList.add("inactive")
+    } else if (a.classList.contains("inactive")) {
+      a.classList.remove("inactive")
+    }
+  })
+}
+
+makeCurrentRouteNotClickable()
+
+// Rewrite development URLs to include .html.
+if (location.port !== "") {
+  $$("nav a[href^='/']").forEach((a) => {
+    const route = new URL(a.href)
+    if (route.pathname !== "/") {
+      a.href = `${a.href}.html`
+    }
+  })
+}
+
+// Don't blink.
 $$("nav a").forEach((a) => {
-  const route = new URL(a.href)
-  if (route.pathname === currentRoute) {
-    a.classList.add("inactive")
-    console.log(a)
-    window.a = a
-  }
+  a.addEventListener("click", (e) => {
+    e.preventDefault()
+    history.pushState({}, "", a.href)
+    fetch(a.href).then((response) => {
+      response.text().then((text) => {
+        const fetchedDocument = document.createElement("html")
+        fetchedDocument.innerHTML = text
+        const body = fetchedDocument.querySelector("body main")
+        $("body main").replaceWith(body)
+        setTimeout(makeCurrentRouteNotClickable, 3000)
+
+        // Rerun initialiser functions.
+        makeCurrentRouteNotClickable()
+      })
+    })
+  })
 })
